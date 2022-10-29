dc9932f53b5c7bf60c9cc2c6c02deec38b2a8f85
Marco RG <marco_rg@msn.com>
2022-10-28 19:08:24 -0500

    Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,19 @@
-
-const {src, dest, watch}= require('gulp');
-const { compile } = require('sass');
-//Watch revisa cambios
-// Dest = destino
-// SRC ubicacion
-
-const sass = require ('gulp-sass')(require('sass'));
-
+const { src, dest, watch } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 
 function css(done){
-// Compila SASS
-// Identificar, compilar, guardar
- src('src/scss/app.scss')
- .pipe( sass({outputStyle: 'compress'}))
- .pipe(dest('build/css'))
+    src('src/scss/app.scss')
+    .pipe(sass())
+    .pipe(postcss([autoprefixer()]))
+    .pipe(dest('build/css'))
 done();
+ 
 }
 
 function dev(){
-
-    watch ('src/scss/app.scss', css);
-
+    watch('src/scss/app.scss', css)
 }
 
 exports.css = css;
