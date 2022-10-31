7a404e11bf101ee0944c7aaf413ffb34a3ced790
Marco RG <marco_rg@msn.com>
2022-10-31 15:15:14 -0600

    Node Eliminado

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,57 @@
 const { src, dest, watch, series } = require('gulp');
+
+//CSS y SASS
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 
+//Imagen
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
+
 function css(done){
-    src('src/scss/app.scss')
+    src('sass/app.scss')
     .pipe(sass({outputStyle:'expanded'}))
-    .pipe( postcss([ autoprefixer()]) )
-    .pipe(dest('build/css'))
-done();
- 
+    .pipe(postcss([autoprefixer()]) )
+    .pipe(dest('css'))
+    done();
+ }
+
+function imagenes(){
+    return src('fotos/**/*')
+    .pipe(imagemin({optimizationLevel: 5}))
+    .pipe(dest('img'));
+}
+
+function imgWebp(){
+    return src('fotos/**/*.{png,jpg,jpeg}')
+    .pipe(webp())
+    .pipe(dest('img'));
+
+}
+
+function minAvif(){
+    const compressAvif = {
+        quality : 50
+    }
+}
+function imgAvif(){
+    return src('fotos/**/*.{png,jpg}')
+    .pipe(avif(minAvif))
+    .pipe(dest('img'));
+
 }
 
 function dev(){
-    watch( 'src/scss/**/*.scss', css );
+    watch('sass/**/*.scss', css);
+    watch('fotos/**/*', imagenes);
 
 }
 
 exports.css = css;
 exports.dev = dev;
-exports.default = series(css, dev);
+exports.imagenes = imagenes;
+exports.imgWebp = imgWebp;
+exports.imgAvif = imgAvif;
+exports.default = series(imagenes, css, dev);
